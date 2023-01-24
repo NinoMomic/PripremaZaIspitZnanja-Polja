d93f5a5dbe06ae80895b85e7fa6d3dd032d1b0f0
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-01-25 00:01:52 +0100

    Add files via upload

---
+++ PripremaZaIspitZnanja/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PripremaZaIspitZnanja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Polje1 = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] Polje2 = { 3, 2, 8, 5, 5, 7 };
+            int BrojClanova, Brojac = 0;
+            if (Polje1.Length < Polje2.Length)
+                BrojClanova = Polje1.Length;
+            else
+                BrojClanova = Polje2.Length;
+            for (int i = 0; i < BrojClanova; i++)
+                if (Polje1[i] == Polje2[i])
+                    Brojac++;
+            Console.WriteLine("Broj zajedničkih članova je " + Brojac);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PripremaZaIspitZnanja13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PripremaZaIspitZnanja13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Polje1 = new int[10];
+            int[] Polje2 = new int[10];
+            string Brojevi;
+            int BrojeviNiz, PrviBroj = 0, DrugiBroj = 0;
+            do
+            {
+                Console.WriteLine("Upišite broj:");
+                Brojevi = Console.ReadLine();
+                BrojeviNiz = Convert.ToInt32(Brojevi);
+                if (BrojeviNiz % 2 != 0)
+                    Polje1[PrviBroj++] = BrojeviNiz;
+                else
+                    Polje2[DrugiBroj++] = BrojeviNiz;
+            } while (BrojeviNiz != 0 && PrviBroj < 10 && DrugiBroj < 10);
+            Console.WriteLine();
+            for (int i = 0; i < PrviBroj; i++)
+                Console.WriteLine(Polje1[i]);
+            Console.WriteLine();
+            for (int i = 0; i < DrugiBroj; i++)
+                Console.WriteLine(Polje2[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
